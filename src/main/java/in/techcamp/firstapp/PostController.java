ec9639ce69400e3d8edb93797f435e5d40cd0bf5
yujimiya <yuji.miyamoto32@gmail.com>
2024-02-17 21:48:12 +0900

    一覧表示機能の学習

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/hello")
@@ -11,5 +13,17 @@ public class PostController {
         var sampleText = "サンプルテキスト";
         model.addAttribute("sampleText", sampleText);
         return "hello";
+    }
+
+    @GetMapping
+//    データを作成し、ビューに渡せるようにする
+    public String showList(Model model){
+        var postList = List.of(
+                new PostEntity(1, "投稿1"),
+                new PostEntity(2, "投稿2"),
+                new PostEntity(3, "投稿3")
+        );
+        model.addAttribute("postList", postList);
+        return "index";
     }
 }
